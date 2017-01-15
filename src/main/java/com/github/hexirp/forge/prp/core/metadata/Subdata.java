6e5f79a2f31de24fa48a2a15d6ad5bac3249fcc3
Hexirp <Hexirp@users.noreply.github.com>
2017-01-15 12:32:21 +0900

    Update MOD's data

--- src/main/java/com/github/hexirp/forge/prp/core/metadata/Subdata.java
+++ src/main/java/com/github/hexirp/forge/prp/core/metadata/Subdata.java
@@ -12,17 +12,17 @@ import net.minecraftforge.fml.common.ModMetadata;
 public class Subdata implements DateSetter {
 	/** MODの解説. */
 	public static final String DESCRIPTION =
-	    "PseudoRedPower-Core is the copied RedPower2 to 1.8.9 or later. So, if you think that this MOD is interesting, respect the author of RedPower2.";
+	    "PseudoRedPower-Core is the salvaged RedPower2 to 1.8.9 or later. So, respect the author of RedPower2 if you think that this MOD is interesting.";
 	
 	/** MODのホームページ. */
-	static final String URL = "https://github.com/Hexirp/PseudoRedPower-Core";
+	public static final String URL = "https://github.com/Hexirp/PseudoRedPower-Core";
 	
 	/**
 	 * @param metadata 設定する対象
 	 */
 	@Override
 	public void set(ModMetadata metadata) {
-		metadata.description = Subdata.DESCRIPTION;
-		metadata.url = Subdata.URL;
+		metadata.description = DESCRIPTION;
+		metadata.url = URL;
 	}
 }
