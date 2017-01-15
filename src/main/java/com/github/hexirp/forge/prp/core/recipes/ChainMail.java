cf2758fbd9363ae3804d9bc3a80bf95e2728f242
Hexirp <Hexirp@users.noreply.github.com>
2017-01-15 12:29:40 +0900

    Update MOD's Recipe

--- src/main/java/com/github/hexirp/forge/prp/core/recipes/ChainMail.java
+++ src/main/java/com/github/hexirp/forge/prp/core/recipes/ChainMail.java
@@ -1,6 +1,10 @@
 package com.github.hexirp.forge.prp.core.recipes;
 
 import com.github.hexirp.forge.RegisterableLibrary;
+import com.github.hexirp.forge.item.MinecraftItem;
+import com.github.hexirp.forge.recipe.builder.RecipeBuilder;
+
+import net.minecraft.init.Items;
 
 /**
  * チェーンメイルを作成するためのレシピを定義する.
@@ -11,7 +15,43 @@ public class ChainMail extends RegisterableLibrary {
 	@Override
 	protected void add() {
 		library
-		    .add(new UpperBody())
-		    .add(new LowerBody());
+		    .add(
+		        new RecipeBuilder()
+		            .product(new MinecraftItem(Items.chainmail_helmet), 1)
+		            .order()
+		            .add(" A ")
+		            .add("A A")
+		            .map()
+		            .put('A', new MinecraftItem(Items.iron_ingot))
+		            .to())
+		    .add(
+		        new RecipeBuilder()
+		            .product(new MinecraftItem(Items.chainmail_chestplate), 1)
+		            .order()
+		            .add("A A")
+		            .add(" A ")
+		            .add("AAA")
+		            .map()
+		            .put('A', new MinecraftItem(Items.iron_ingot))
+		            .to())
+		    .add(
+		        new RecipeBuilder()
+		            .product(new MinecraftItem(Items.chainmail_leggings), 1)
+		            .order()
+		            .add(" A ")
+		            .add("A A")
+		            .add("A A")
+		            .map()
+		            .put('A', new MinecraftItem(Items.iron_ingot))
+		            .to())
+		    .add(
+		        new RecipeBuilder()
+		            .product(new MinecraftItem(Items.chainmail_boots), 1)
+		            .order()
+		            .add("A  ")
+		            .add("  A")
+		            .map()
+		            .put('A', new MinecraftItem(Items.iron_ingot))
+		            .to());
 	}
 }
