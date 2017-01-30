8d7edb4016d3fb6ee938dd8ac8353e8fd81cbc9c
Hexirp <Hexirp@users.noreply.github.com>
2017-01-30 19:51:48 +0900

    Add SmeltingExpPhase

---
+++ src/main/java/com/github/hexirp/forge/recipe/builder/SmeltingExpPhase.java
@@ -0,0 +1,36 @@
+package com.github.hexirp.forge.recipe.builder;
+
+import com.github.hexirp.builder.SimpleTransitional;
+import com.github.hexirp.builder.StackedPhase;
+import com.github.hexirp.forge.smelting.SmeltingRecipe;
+import com.github.hexirp.forge.smelting.SmeltingRecipeExp;
+
+/**
+ * 製錬レシピの経験値を定義する段階を表現する.
+ *
+ * @author Hexirp
+ */
+public class SmeltingExpPhase
+    extends
+        StackedPhase<SmeltingIngaPhase>
+    implements
+        SimpleTransitional<SmeltingRecipe> {
+	/** 製錬レシピの経験値. */
+	private final SmeltingRecipeExp exp;
+	
+	/**
+	 * Setter.
+	 *
+	 * @param phase 前段階
+	 * @param exp 経験値
+	 */
+	public SmeltingExpPhase(SmeltingIngaPhase phase, SmeltingRecipeExp exp) {
+		super(phase);
+		this.exp = exp;
+	}
+	
+	@Override
+	public SmeltingRecipe to() {
+		return new SmeltingRecipe(previous().previous(), exp);
+	}
+}
