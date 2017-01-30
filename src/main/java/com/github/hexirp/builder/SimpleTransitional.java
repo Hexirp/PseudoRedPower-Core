9af5d14b2354ec35963831b8561e3a2e3c45e546
Hexirp <Hexirp@users.noreply.github.com>
2017-01-30 18:20:53 +0900

    Clean SimpleTransitional

--- src/main/java/com/github/hexirp/builder/SimpleTransitional.java
+++ src/main/java/com/github/hexirp/builder/SimpleTransitional.java
@@ -4,13 +4,13 @@ package com.github.hexirp.builder;
  * 次の段階が一つしかない段階を表現する.
  *
  * @author Hexirp
- * @param <Product> 生成物の型
+ * @param <Next> 生成物の型
  */
-public interface SimpleTransitional<Product> {
+public interface SimpleTransitional<Next> {
 	/**
 	 * 次の段階に進む.
 	 *
 	 * @return 生成物
 	 */
-	public Product to();
+	public Next to();
 }
