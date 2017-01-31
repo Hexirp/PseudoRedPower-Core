60787d463d6606c1786d619fd98e776b480a5cb4
Hexirp <Hexirp@users.noreply.github.com>
2017-01-31 09:21:05 +0900

    Clean LoadableItem

--- src/main/java/com/github/hexirp/forge/item/LoadableItem.java
+++ src/main/java/com/github/hexirp/forge/item/LoadableItem.java
@@ -5,9 +5,7 @@ import com.github.hexirp.forge.Index;
 import com.github.hexirp.forge.Loadable;
 
 import net.minecraft.item.Item;
-import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.common.registry.GameRegistry;
-import net.minecraftforge.fml.relauncher.Side;
 
 /**
  * {@link Loadable}である{@link Item}を表現する.
@@ -17,7 +15,7 @@ import net.minecraftforge.fml.relauncher.Side;
  *
  * @author Hexirp
  */
-public class LoadableItem implements Loadable, Named {
+public class LoadableItem implements Loadable {
 	/** アイテムの名前. */
 	private final String name;
 	
@@ -39,17 +37,8 @@ public class LoadableItem implements Loadable, Named {
 	public Index<MinecraftItem> load(Environment env) {
 		GameRegistry.registerItem(item, name);
 		
-		// TODO: 綺麗にする
-		if (env.side() == Side.CLIENT) ModelLoader.setCustomModelResourceLocation(
-		    item,
-		    0,
-		    env.location(name, "inventory"));
+		new ItemResourceLocation(env).register(item, name);
 		
 		return new Index<MinecraftItem>().put(name, new MinecraftItem(item));
-	}
-	
-	@Override
-	public String name() {
-		return name;
 	}
 }
