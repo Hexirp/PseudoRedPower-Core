7bb6706bd194ee72b4de340f50f3b3294d96910e
Hexirp <Hexirp@users.noreply.github.com>
2017-02-03 16:11:49 +0900

    Clean ItemResourceLocation

--- src/main/java/com/github/hexirp/forge/item/LoadableItem.java
+++ src/main/java/com/github/hexirp/forge/item/LoadableItem.java
@@ -34,7 +34,7 @@ public class LoadableItem implements Loadable {
 	public Index<MinecraftItem> load(final Environment env) {
 		GameRegistry.registerItem(i.get(), i.name());
 		
-		new ItemResourceLocation(env).register(i);
+		new ItemResourceLocation(env).set(i);
 		
 		return new Index<MinecraftItem>().put(i.name(), new MinecraftItem(i.get()));
 	}
