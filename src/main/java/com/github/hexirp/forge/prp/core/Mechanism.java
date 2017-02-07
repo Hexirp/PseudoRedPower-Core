e2a10c0ef5c24d87bfd51ab175a0122b228fce63
Hexirp <Hexirp@users.noreply.github.com>
2017-02-07 20:23:18 +0900

    Register SilverOre

--- src/main/java/com/github/hexirp/forge/prp/core/Mechanism.java
+++ src/main/java/com/github/hexirp/forge/prp/core/Mechanism.java
@@ -1,8 +1,16 @@
 package com.github.hexirp.forge.prp.core;
 
+import com.github.hexirp.annotation.Nullable;
 import com.github.hexirp.forge.Index;
 import com.github.hexirp.forge.item.Environment;
 import com.github.hexirp.forge.item.MinecraftItem;
+import com.github.hexirp.forge.prp.core.blocks.SilverOre;
+
+import net.minecraft.block.Block;
+import net.minecraft.client.resources.model.ModelResourceLocation;
+import net.minecraft.item.Item;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 
 /**
  * PRP-Coreにより追加されるメカニズム群、つまりアイテムやレシピなどを表現する.
@@ -10,6 +18,10 @@ import com.github.hexirp.forge.item.MinecraftItem;
  * @author Hexirp
  */
 public class Mechanism {
+	/** 銀鉱石. */
+	@Nullable
+	private static Block silver_ore;
+	
 	/**
 	 * PRP-Coreのメカニズム群を追加する.
 	 *
@@ -19,5 +31,12 @@ public class Mechanism {
 		final Index<MinecraftItem> items = new Items().load(env);
 		
 		new Recipes(items).register();
+		
+		silver_ore = new SilverOre();
+		GameRegistry.registerBlock(silver_ore, "silver_ore");
+		ModelLoader.setCustomModelResourceLocation(
+		    Item.getItemFromBlock(silver_ore),
+		    0,
+		    new ModelResourceLocation(Metadata.MOD_ID + ":" + "silver_ore", "inventory"));
 	}
 }
