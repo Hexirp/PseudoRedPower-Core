4335a02883712adb592965238bd2ac4a9bed14eb
Hexirp <Hexirp@users.noreply.github.com>
2017-02-22 18:03:36 +0900

    Add Ores

--- src/main/java/hexirp/forge/prp/core/Mechanism.java
+++ src/main/java/hexirp/forge/prp/core/Mechanism.java
@@ -16,9 +16,8 @@ public class Mechanism {
 	 * @param env 追加する環境
 	 */
 	public void add(final Environment env) {
-		final Index<MinecraftItem> items = new Items().load(env);
-		final Index<MinecraftItem> blocks = new Blocks().load(env);
-		
-		new Recipes(items.merge(blocks)).register();
+		final Index<MinecraftItem> ores = new Ores().load(env);
+
+		new Recipes(ores).register();
 	}
 }
