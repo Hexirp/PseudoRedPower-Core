e2f268804628ca406515bade15f68d57de9dfed1
Hexirp <Hexirp@users.noreply.github.com>
2017-02-24 17:31:00 +0900

    Add TestUtil

---
+++ src/test/java/hexirp/test/TestUtil.java
@@ -0,0 +1,24 @@
+package hexirp.test;
+
+import static org.junit.Assert.*;
+
+import java.util.function.Function;
+
+/**
+ * メソッドチェーンに関するテストの為の関数を記述する.
+ *
+ * @author Hexirp
+ */
+public class TestUtil {
+	/**
+	 * メソッドチェーンに副作用がある.
+	 *
+	 * @param data データ
+	 * @param method メソッドチェーン
+	 */
+	public static <A> void method_chain_with_side_effects(final A data, final Function<A, A> method) {
+		final A data2 = method.apply(data);
+		
+		assertEquals(data, data2);
+	}
+}
---
+++ src/test/java/hexirp/test/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * テストのための道具を記述する.
+ *
+ * @author Hexirp
+ */
+@NonNullByDefault
+package hexirp.test;
+
+import hexirp.annotation.NonNullByDefault;
