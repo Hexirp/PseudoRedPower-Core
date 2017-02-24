f8f7f8b3de24ade881f4e9276fa3893601e23f88
Hexirp <Hexirp@users.noreply.github.com>
2017-02-24 17:31:40 +0900

    Update ShapedRecipeMaterialMapTest

--- src/test/java/hexirp/forge/recipe/test/ShapedRecipeMaterialMapTest.java
+++ src/test/java/hexirp/forge/recipe/test/ShapedRecipeMaterialMapTest.java
@@ -1,9 +1,11 @@
 package hexirp.forge.recipe.test;
 
+import static hexirp.test.TestUtil.*;
 import static org.junit.Assert.*;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.function.Supplier;
 
 import org.junit.Test;
 
@@ -16,14 +18,15 @@ import hexirp.forge.test.MinecraftItemMock;
  * @see hexirp.forge.recipe.ShapedRecipeMaterialMap
  */
 public class ShapedRecipeMaterialMapTest {
+	/** インスタンス生成機. */
+	private static final Supplier<ShapedRecipeMaterialMap> spmm = ShapedRecipeMaterialMap::new;
+	
 	/**
 	 * コントストラクタは一定の内容を生成する.
 	 */
 	@Test
 	public void test_ShapedRecipeMaterialMap() {
-		assertEquals(
-		    new ShapedRecipeMaterialMap().list(),
-		    new ShapedRecipeMaterialMap().list());
+		assertEquals(spmm.get().list(), spmm.get().list());
 	}
 	
 	/**
@@ -31,9 +34,7 @@ public class ShapedRecipeMaterialMapTest {
 	 */
 	@Test
 	public void test_toList() {
-		assertEquals(
-		    new ShapedRecipeMaterialMap().list(),
-		    new LinkedList<>());
+		assertEquals(spmm.get().list(), new LinkedList<>());
 	}
 	
 	/**
@@ -41,10 +42,7 @@ public class ShapedRecipeMaterialMapTest {
 	 */
 	@Test
 	public void test_put_effect() {
-		final ShapedRecipeMaterialMap testee1 = new ShapedRecipeMaterialMap();
-		final ShapedRecipeMaterialMap testee2 = testee1.put('A', new MinecraftItemMock("acacia_door"));
-		
-		assertEquals(testee1, testee2);
+		method_chain_with_side_effects(spmm.get(), t -> t.put('A', new MinecraftItemMock("acacia_door")));
 	}
 	
 	/**
@@ -52,12 +50,9 @@ public class ShapedRecipeMaterialMapTest {
 	 */
 	@Test
 	public void test_put() {
-		final List<Object> testee1 = new ShapedRecipeMaterialMap()
-		    .put('A', new MinecraftItemMock("acacia_door"))
-		    .list();
+		final List<Object> testee1 = spmm.get().put('A', new MinecraftItemMock("acacia_door")).list();
 		
 		final LinkedList<Object> testee2 = new LinkedList<>();
-		
 		testee2.add('A');
 		testee2.add(new ItemMock("acacia_door"));
 		
