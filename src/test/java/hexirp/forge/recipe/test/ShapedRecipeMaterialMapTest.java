10684ebec76a9a57ff44d428b73957b2d6c75f89
Hexirp <Hexirp@users.noreply.github.com>
2017-02-25 18:14:22 +0900

    Update ShapedRecipeMaterialTest

--- src/test/java/hexirp/forge/recipe/test/ShapedRecipeMaterialMapTest.java
+++ src/test/java/hexirp/forge/recipe/test/ShapedRecipeMaterialMapTest.java
@@ -17,40 +17,40 @@ import hexirp.forge.test.MinecraftItemMock;
  * @author Hexirp
  * @see hexirp.forge.recipe.ShapedRecipeMaterialMap
  */
-public class ShapedRecipeMaterialMapTest {
+public final class ShapedRecipeMaterialMapTest {
 	/** インスタンス生成機. */
-	private static final Supplier<ShapedRecipeMaterialMap> spmm = ShapedRecipeMaterialMap::new;
+	private static final Supplier<ShapedRecipeMaterialMap> testee = ShapedRecipeMaterialMap::new;
 	
 	/**
 	 * コントストラクタは一定の内容を生成する.
 	 */
 	@Test
-	public void test_ShapedRecipeMaterialMap() {
-		assertEquals(spmm.get().list(), spmm.get().list());
+	public static final void test_ShapedRecipeMaterialMap() {
+		assertEquals(testee.get().list(), testee.get().list());
 	}
 	
 	/**
 	 * コントストラクタで生成される内容は空のマップである.
 	 */
 	@Test
-	public void test_toList() {
-		assertEquals(spmm.get().list(), new LinkedList<>());
+	public static final void test_toList() {
+		assertEquals(testee.get().list(), new LinkedList<>());
 	}
 	
 	/**
 	 * putは副作用を持つ.
 	 */
 	@Test
-	public void test_put_effect() {
-		method_chain_with_side_effects(spmm.get(), t -> t.put('A', new MinecraftItemMock("acacia_door")));
+	public static final void test_put_effect() {
+		method_chain_with_side_effects(testee.get(), t -> t.put('A', new MinecraftItemMock("acacia_door")));
 	}
 	
 	/**
 	 * putは内部のマップに値を追加する.
 	 */
 	@Test
-	public void test_put() {
-		final List<Object> testee1 = spmm.get().put('A', new MinecraftItemMock("acacia_door")).list();
+	public static final void test_put() {
+		final List<Object> testee1 = testee.get().put('A', new MinecraftItemMock("acacia_door")).list();
 		
 		final LinkedList<Object> testee2 = new LinkedList<>();
 		testee2.add('A');
