b042aaa1fa1486c44738ee916cf8fa7ba04674ca
Hexirp <Hexirp@users.noreply.github.com>
2017-03-04 10:50:44 +0900

    Update Javadoc in MethodTest

--- src/test/java/hexirp/test/MethodTest.java
+++ src/test/java/hexirp/test/MethodTest.java
@@ -5,12 +5,13 @@ import static org.junit.Assert.*;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
+import hexirp.annotation.Method;
 import hexirp.annotation.Method.Chaining;
 import hexirp.annotation.Method.Getting;
 import hexirp.annotation.Method.Setting;
 
 /**
- * メソッドをテストする為の関数を記述する.
+ * メソッドが{@link Method}を満たすかテストする為の関数を記述する.
  *
  * @author Hexirp
  */
@@ -23,6 +24,7 @@ public class MethodTest {
 	 * </p>
 	 *
 	 * @param constructor コントストラクタを表現するラムダ式
+	 * @param <A> テストするクラス
 	 */
 	public static <A> void assertSetting(final Supplier<A> constructor) {
 		assertEquals(constructor.get(), constructor.get());
@@ -31,8 +33,14 @@ public class MethodTest {
 	/**
 	 * メソッドが{@link Getting}を満たす.
 	 *
+	 * <p>
+	 * 使用例: {@code assertGetting(new Foo(), t -> t.baa()}
+	 * </p>
+	 *
 	 * @param data データ
 	 * @param method メソッドを表すラムダ式
+	 * @param <A> テストするクラス
+	 * @param <B> 取り出す情報
 	 */
 	public static <A, B> void assertGetting(final A data, final Function<A, B> method) {
 		final B data1 = method.apply(data);
@@ -50,7 +58,7 @@ public class MethodTest {
 	 *
 	 * @param data データ
 	 * @param method メソッドを表すラムダ式
-	 * @param <A> 任意のクラス
+	 * @param <A> テストするクラス
 	 */
 	public static <A> void assertChaining(final A data, final Function<A, A> method) {
 		final A new_data = method.apply(data);
