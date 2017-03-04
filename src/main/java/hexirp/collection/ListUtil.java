96ad2e046a0dd422e37f7ee205bb932c5150e89d
Hexirp <Hexirp@users.noreply.github.com>
2017-03-04 12:12:03 +0900

    Update classes in hexirp.* to use Method's type

--- src/main/java/hexirp/collection/ListUtil.java
+++ src/main/java/hexirp/collection/ListUtil.java
@@ -4,6 +4,9 @@ import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
 
+import hexirp.annotation.Method.Chaining;
+import hexirp.annotation.Method.Getting;
+
 /**
  * 配列への変換を表現する.
  *
@@ -13,24 +16,26 @@ import java.util.List;
 public class ListUtil<Element> {
 	/** 内部に保持するリスト. */
 	private final List<Element> list = new LinkedList<>();
-	
+
 	/**
 	 * 配列に変換する.
 	 *
 	 * @return 配列に変換されたリスト
 	 */
+	@Getting
 	public Object[] array() {
 		final Object[] ret = list.toArray();
 		if (null == ret) throw null;
 		return ret;
 	}
-	
+
 	/**
 	 * リストを結合する.
 	 *
 	 * @param col コレクション
 	 * @return 自分自身
 	 */
+	@Chaining
 	public ListUtil<Element> merge(final Collection<? extends Element> col) {
 		list.addAll(col);
 		return this;
