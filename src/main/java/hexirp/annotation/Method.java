37450d2cb45b318b052f7f3d116837415fe465fb
Hexirp <Hexirp@users.noreply.github.com>
2017-03-07 20:45:53 +0900

    Add Command

--- src/main/java/hexirp/annotation/Method.java
+++ src/main/java/hexirp/annotation/Method.java
@@ -41,4 +41,14 @@ public @interface Method {
 	@Retention(RetentionPolicy.SOURCE)
 	@Target(ElementType.METHOD)
 	public @interface Chaining {}
+	
+	/**
+	 * メソッドが副作用を持ち、その他の分類に当てはまらないことを表す. 使用するときは時系列や実行環境に注意を要する。
+	 * 名前は動詞であることが望ましい。
+	 *
+	 * @author Hexirp
+	 */
+	@Retention(RetentionPolicy.SOURCE)
+	@Target(ElementType.METHOD)
+	public @interface Command {}
 }
