d8c7008362bf3ae758fe196b3afcac84561920b3
Hexirp <Hexirp@users.noreply.github.com>
2017-03-10 12:57:50 +0900

    Add AbstractListUtil
    使用するリストをオーバーライドで自由に切り替えられるListUtilを作成

--- src/main/java/hexirp/collection/ListUtil.java
+++ src/main/java/hexirp/collection/ListUtil.java
@@ -1,10 +1,8 @@
 package hexirp.collection;
 
-import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
 
-import hexirp.annotation.Method.Chaining;
 import hexirp.annotation.Method.Getting;
 
 /**
@@ -13,35 +11,11 @@ import hexirp.annotation.Method.Getting;
  * @author Hexirp
  * @param <Element> 要素
  */
-public class ListUtil<Element> {
-	/** 内部に保持するリスト. */
-	private final List<Element> list = usedList();
-	
-	/**
-	 * @return 配列に変換されたリスト
-	 */
-	@Getting
-	public Object[] array() {
-		final Object[] ret = list.toArray();
-		if (null == ret) throw null;
-		return ret;
-	}
-	
-	/**
-	 * リストを結合する.
-	 *
-	 * @param col コレクション
-	 * @return this
-	 */
-	@Chaining
-	public ListUtil<Element> merge(final Collection<? extends Element> col) {
-		list.addAll(col);
-		return this;
-	}
-	
+public class ListUtil<Element> extends AbstractListUtil<Element> {
 	/**
 	 * @return 使用するリスト
 	 */
+	@Override
 	@Getting
 	public List<Element> usedList() {
 		return new LinkedList<>();
