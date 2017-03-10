058d8e83e23c9d537cba7af823b9f42f71555958
Hexirp <Hexirp@users.noreply.github.com>
2017-03-10 13:04:02 +0900

    Add UsingLinkedList
    LinkedListを使用する事を示すインターフェースを作成。コンパイル通らず

--- src/main/java/hexirp/collection/ListUtil.java
+++ src/main/java/hexirp/collection/ListUtil.java
@@ -1,24 +1,9 @@
 package hexirp.collection;
 
-import java.util.LinkedList;
-import java.util.List;
-
-import hexirp.annotation.Method.Getting;
-
 /**
  * 配列への変換を表現する.
  *
  * @author Hexirp
  * @param <Element> 要素
  */
-public class ListUtil<Element> extends AbstractListUtil<Element> {
-	/**
-	 * @return 使用するリスト
-	 */
-	@Override
-	@Getting
-	public List<Element> usedList() {
-		return new LinkedList<>();
-		
-	}
-}
+public class ListUtil<Element> extends AbstractListUtil<Element> implements UsingLinkedList<Element> {}
---
+++ src/main/java/hexirp/collection/UsingLinkedList.java
@@ -0,0 +1,22 @@
+package hexirp.collection;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import hexirp.annotation.Method.Getting;
+
+/**
+ * {@link LinkedList}を使用する事を表現する.
+ *
+ * @author Hexirp
+ * @param <Element> 要素
+ */
+public interface UsingLinkedList<Element> {
+	/**
+	 * @return 使用するリスト
+	 */
+	@Getting
+	default public List<Element> usedList() {
+		return new LinkedList<>();
+	}
+}
