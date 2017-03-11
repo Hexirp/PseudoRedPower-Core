30ebfaa4b1a25f49d7f508caccd8666072bd24e5
Hexirp <Hexirp@users.noreply.github.com>
2017-03-11 12:12:55 +0900

    Add OrderPhase
    リスト系のBuilderに対する実装を探るため

---
+++ src/main/java/hexirp/forge/recipe/builder/OrderPhase.java
@@ -0,0 +1,39 @@
+package hexirp.forge.recipe.builder;
+
+import hexirp.annotation.Method.Chaining;
+import hexirp.annotation.Method.Setting;
+import hexirp.forge.recipe.RecipeProduct;
+import hexirp.forge.recipe.ShapedRecipeAbstractOrder;
+
+/**
+ * 並び順を定義する段階を表現する.
+ *
+ * @author Hexirp
+ */
+public class OrderPhase {
+	/** 段階の記録. */
+	protected final RecipeProduct stack;
+	
+	/** 保存する値. */
+	protected final ShapedRecipeAbstractOrder value = new ShapedRecipeAbstractOrder();
+	
+	/**
+	 * @param stack {@link #stack}
+	 */
+	@Setting
+	public OrderPhase(final RecipeProduct stack) {
+		this.stack = stack;
+	}
+	
+	/**
+	 * 要素を追加する.
+	 *
+	 * @param element 要素
+	 * @return this
+	 */
+	@Chaining
+	public OrderPhase set(final String element) {
+		value.add(element);
+		return this;
+	}
+}
