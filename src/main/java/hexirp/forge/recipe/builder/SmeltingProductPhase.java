2e893d073c918aac8ee3439c3ae8c2c50ca9b59c
Hexirp <Hexirp@users.noreply.github.com>
2017-03-11 15:50:09 +0900

    Add SmeltingProductPhase

---
+++ src/main/java/hexirp/forge/recipe/builder/SmeltingProductPhase.java
@@ -0,0 +1,26 @@
+package hexirp.forge.recipe.builder;
+
+import hexirp.annotation.Method.Setting;
+import hexirp.builder.Phase;
+import hexirp.collection.Unit;
+import hexirp.forge.MinecraftItem;
+
+/**
+ * 製錬レシピの結果を定義する段階を表現する.
+ *
+ * @author Hexirp
+ */
+public class SmeltingProductPhase extends Phase<Unit, MinecraftItem, SmeltingMaterialPhase, MinecraftItem> {
+	/**
+	 * @param value 保存する値
+	 */
+	@Setting
+	public SmeltingProductPhase(final MinecraftItem value) {
+		super(Unit.$(), value);
+	}
+	
+	@Override
+	public SmeltingMaterialPhase to(final MinecraftItem value) {
+		return new SmeltingMaterialPhase(second(), value);
+	}
+}
