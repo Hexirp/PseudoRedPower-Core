b7c35396ec9b15a4d0109e5034ccf53d2cf2c386
Hexirp <Hexirp@users.noreply.github.com>
2017-03-11 16:17:15 +0900

    Update RecipeBuilder

--- src/main/java/hexirp/forge/recipe/builder/RecipeBuilder.java
+++ src/main/java/hexirp/forge/recipe/builder/RecipeBuilder.java
@@ -2,7 +2,7 @@ package hexirp.forge.recipe.builder;
 
 import hexirp.annotation.Method.Getting;
 import hexirp.annotation.Method.Setting;
-import hexirp.builder.Phase;
+import hexirp.builder.BranchedPhase;
 import hexirp.collection.Unit;
 import hexirp.forge.MinecraftItem;
 
@@ -11,13 +11,13 @@ import hexirp.forge.MinecraftItem;
  *
  * @author Hexirp
  */
-public class RecipeBuilder extends Phase<Unit, Unit, OutputPhase, MinecraftItem> {
+public class RecipeBuilder extends BranchedPhase<Unit, Unit, OutputPhase, MinecraftItem, SmeltingProductPhase, MinecraftItem> {
 	/** Setting. */
 	@Setting
 	public RecipeBuilder() {
 		super(Unit.$(), Unit.$());
 	}
-	
+
 	/**
 	 * 製錬レシピの因果関係を定義する段階に移行する.
 	 *
@@ -26,13 +26,19 @@ public class RecipeBuilder extends Phase<Unit, Unit, OutputPhase, MinecraftItem>
 	 * @return 次の段階
 	 * @see SmeltingIngaPhase#SmeltingIngaPhase(MinecraftItem, MinecraftItem)
 	 */
+	@Deprecated
 	public SmeltingIngaPhase inga(final MinecraftItem product, final MinecraftItem material) {
 		return new SmeltingIngaPhase(product, material);
 	}
-	
+
 	@Override
 	@Getting
 	public OutputPhase to(final MinecraftItem value) {
 		return new OutputPhase(value);
+	}
+	
+	@Override
+	public SmeltingProductPhase to2(final MinecraftItem value) {
+		return new SmeltingProductPhase(value);
 	}
 }
