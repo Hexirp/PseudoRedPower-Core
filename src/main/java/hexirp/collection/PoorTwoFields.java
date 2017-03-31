028883853c68ea947f83f4e84f45869c67607022
Hexirp <Hexirp@users.noreply.github.com>
2017-03-31 13:36:29 +0900

    Add PoorTwoFields

---
+++ src/main/java/hexirp/collection/PoorTwoFields.java
@@ -0,0 +1,31 @@
+package hexirp.collection;
+
+import hexirp.annotation.Nullable;
+import hexirp.annotation.Setting;
+
+/**
+ * 機能を持たない{@link TwoFields}を表現する.
+ *
+ * @author Hexirp
+ *
+ * @param <T1> 型1
+ * @param <T2> 型2
+ */
+public class PoorTwoFields<T1, T2> {
+	/** 値1. */
+	protected final T1 first;
+	
+	/** 値2. */
+	protected final T2 second;
+	
+	/**
+	 * @param first {@link #first}
+	 * @param second {@link #second}
+	 */
+	@Setting
+	public PoorTwoFields(final @Nullable T1 first, final @Nullable T2 second) {
+		if (null == first || null == second) throw null;
+		this.first = first;
+		this.second = second;
+	}
+}
--- src/main/java/hexirp/collection/TwoFields.java
+++ src/main/java/hexirp/collection/TwoFields.java
@@ -13,22 +13,14 @@ import hexirp.annotation.Setting;
  * @param <T1> 型1
  * @param <T2> 型2
  */
-public abstract class TwoFields<T1, T2> {
-	/** 値1. */
-	protected final T1 first;
-	
-	/** 値2. */
-	protected final T2 second;
-	
+public abstract class TwoFields<T1, T2> extends PoorTwoFields<T1, T2> {
 	/**
 	 * @param first {@link #first}
 	 * @param second {@link #second}
 	 */
 	@Setting
 	public TwoFields(final @Nullable T1 first, final @Nullable T2 second) {
-		if (null == first || null == second) throw null;
-		this.first = first;
-		this.second = second;
+		super(first, second);
 	}
 	
 	@Override
