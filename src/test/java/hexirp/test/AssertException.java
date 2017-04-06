7d20f1bf017dec8070ee5650e8833b3d4ffc86c9
Hexirp <Hexirp@users.noreply.github.com>
2017-04-06 17:04:43 +0900

    Delete unused methods

--- src/test/java/hexirp/test/AssertException.java
+++ src/test/java/hexirp/test/AssertException.java
@@ -20,19 +20,4 @@ public final class AssertException {
 			assertTrue("Thrown " + e.toString() + " isn't " + exclass.toString(), exclass.isInstance(e));
 		}
 	}
-	
-	/**
-	 * @param runner 実行する処理
-	 * @param exclass 投げられるべき例外
-	 * @param exmessage 例外のメッセージ
-	 */
-	public static final void assertException(final Runnable runner, final Class<? extends Exception> exclass, final String exmessage) {
-		try {
-			runner.run();
-			fail(exclass.toString() + " wasn't thrown");
-		} catch (final Exception e) {
-			assertTrue("Thrown " + e.toString() + " isn't " + exclass.toString(), exclass.isInstance(e));
-			assertEquals(e.getMessage(), exmessage);
-		}
-	}
 }
