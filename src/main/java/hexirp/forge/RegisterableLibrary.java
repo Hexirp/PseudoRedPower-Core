f0c64a00ffd15a0d545286096393dd13509a1b3d
Hexirp <Hexirp@users.noreply.github.com>
2017-04-27 21:17:00 +0900

    Clean Javadoc

--- src/main/java/hexirp/forge/RegisterableLibrary.java
+++ src/main/java/hexirp/forge/RegisterableLibrary.java
@@ -6,8 +6,6 @@ import hexirp.annotation.Command;
 /**
  * 要素一つ一つを登録することが出来る{@link Library}を表現する.
  *
- * レシピを追加するクラスを抽象化することにより作成された。
- *
  * @author Hexirp
  */
 public class RegisterableLibrary extends Library<Registerable> implements Registerable {
