fe0d21acab4205b3811f1be6c0856612651d5ffa
Hexirp <Hexirp@users.noreply.github.com>
2017-04-27 19:15:23 +0900

    Clean Javadoc in LoadableLibrary

--- src/main/java/hexirp/forge/LoadableLibrary.java
+++ src/main/java/hexirp/forge/LoadableLibrary.java
@@ -6,8 +6,6 @@ import hexirp.forge.loadable.Environment;
 /**
  * 要素一つ一つを読み込ませることが出来る{@link Library}を表現する.
  *
- * アイテムを追加するクラスを抽象化することにより作成された。
- *
  * @author Hexirp
  */
 public class LoadableLibrary extends Library<Loadable> implements Loadable {
