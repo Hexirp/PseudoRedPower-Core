2b27a21a3730117215f7409f692ea939cdc254fc
Hexirp <Hexirp@users.noreply.github.com>
2017-04-28 18:16:20 +0900

    Update ArrayBuilder
    ArrayBuilderにOneFieldを継承させるためにAbstractListUtilの継承関係を取り除いた

--- src/main/java/hexirp/collection/ArrayBuilder.java
+++ src/main/java/hexirp/collection/ArrayBuilder.java
@@ -1,9 +1,45 @@
 package hexirp.collection;
 
+import java.util.Collection;
+import java.util.List;
+
+import hexirp.annotation.Chaining;
+import hexirp.annotation.Getting;
+import hexirp.annotation.Setting;
+
 /**
  * 配列への変換を表現する.
  *
  * @author Hexirp
  * @param <Element> 要素
  */
-public class ArrayBuilder<Element> extends AbstractListUtil<Element> implements UsingLinkedList<Element> {}
+public class ArrayBuilder<Element> extends OneField<List<Element>> {
+	/** Setting. */
+	@Setting
+	public ArrayBuilder() {
+		super(new LinkedList<Element>());
+	}
+
+	/**
+	 * @return 配列に変換されたリスト
+	 */
+	@Getting
+	public Object[] array() {
+		final Object[] ret = value.toArray();
+		if (null == ret) throw null;
+		return ret;
+	}
+	
+	/**
+	 * リストを結合する.
+	 *
+	 * @param col コレクション
+	 * @return this
+	 */
+	@Chaining
+	public AbstractListUtil<Element> merge(final Collection<? extends Element> col) {
+		value.addAll(col);
+		
+		return this;
+	}
+}
