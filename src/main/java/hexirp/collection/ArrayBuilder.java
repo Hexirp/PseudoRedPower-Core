0283d1dcf2dc3842b5b60155e43e5ba2da75c3d3
Hexirp <Hexirp@users.noreply.github.com>
2017-04-28 18:27:17 +0900

    Fix ArrayBuilder
    Ref https://travis-ci.org/Hexirp/PseudoRedPower-Core/builds/226741885

--- src/main/java/hexirp/collection/ArrayBuilder.java
+++ src/main/java/hexirp/collection/ArrayBuilder.java
@@ -2,6 +2,7 @@ package hexirp.collection;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.LinkedList;
 
 import hexirp.annotation.Chaining;
 import hexirp.annotation.Getting;
